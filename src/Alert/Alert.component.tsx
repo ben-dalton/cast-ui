3daa26794fb112331bc221a49ef59af372e92c5e
Dantiff <20873103+Dantiff@users.noreply.github.com>
2020-10-09 20:20:11 +0300

    fix(alert): undo test code
    Undo test code

--- src/Alert/Alert.component.tsx
+++ src/Alert/Alert.component.tsx
@@ -46,7 +46,7 @@ const SAlert = styled.div`
     props.lightMode
       ? props.theme.alert.lightFontWeight
       : props.theme.alert.fontWeight};
-  line-height: 1;
+  line-height: ${(props: Props) => props.theme.alert.lineHeight};
 `;
 
 export const Alert: React.FunctionComponent<Props> = ({
